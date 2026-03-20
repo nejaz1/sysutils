0182104eb435753eefd598608b319e7f376dc077
nejaz1 <11272731+nejaz1@users.noreply.github.com>
2026-03-21 00:38:34 +0100

    Cleanup of transcribe, can use google gemini for audio transcription and speaker diatization

--- installers/install_brew.sh
+++ installers/install_brew.sh
@@ -1,3 +1,12 @@
 # Install homebrew
 set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+# Install brew packages through formulae and casks
+
+# Formulae
+brew install git node pandoc pnpm uv
+
+# Casks
+brew install --cask audio-hijack chronosync claude loopback obsidian proton-drive proton-pass rectangle soundsource visual-studio-code zotero
