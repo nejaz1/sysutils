c10b22611fc99f87c47058a4c3ace9c74a67e9e1
nejaz1 <11272731+nejaz1@users.noreply.github.com>
2026-04-05 15:10:56 +0200

    Updated brew installer, added fonts + new apps

--- installers/install_brew.sh
+++ installers/install_brew.sh
@@ -5,8 +5,31 @@ set -e
 
 # Install brew packages through formulae and casks
 
+###############################################################
 # Formulae
+# Programming languages
 brew install git node pandoc pnpm uv
 
 # Casks
-brew install --cask audio-hijack chronosync claude loopback obsidian proton-drive proton-pass rectangle soundsource visual-studio-code zotero
+# Audio
+brew install --cask audio-hijack loopback soundsource
+
+# Programming
+brew install --cask claude claude-code visual-studio-code 
+
+# Security
+brew install --cask proton-drive proton-pass
+brew install protonpass/tap/pass-cli
+
+# Notes
+brew install --cask obsidian zotero granola
+
+# System
+brew install --cask chronosync rectangle google-chrome
+
+
+###############################################################
+# Fonts
+brew install --cask font-open-sans font-dm-sans
+brew install --cask font-ia-writer-duo font-ia-writer-mono font-ia-writer-quattro
+
