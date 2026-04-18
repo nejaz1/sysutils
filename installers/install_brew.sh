179ed5bb1e8dfd5738633d7587052cf661dc3d34
nejaz1 <11272731+nejaz1@users.noreply.github.com>
2026-04-18 17:33:21 +0200

    updated install_brew.sh

--- installers/install_brew.sh
+++ installers/install_brew.sh
@@ -2,34 +2,34 @@
 set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# --- System ---
+brew install --cask chronosync rectangle ghostty
 
-# Install brew packages through formulae and casks
-
-###############################################################
-# Formulae
-# Programming languages
-brew install git node pandoc pnpm pixi uv
-
-# Casks
-# Audio
-brew install --cask audio-hijack loopback soundsource
-
-# Programming
-brew install --cask claude claude-code visual-studio-code cursor r rustup 
-
-# Security
-brew install --cask proton-drive proton-pass
+# --- Security ---
 brew install protonpass/tap/pass-cli
+brew install --cask proton-drive proton-pass
 
-# Notes
+# --- Package managers ---
+brew install git glab gitlab-ci-local node pandoc pnpm pixi uv rustup
+
+# --- IDEs ---
+brew install --cask cursor visual-studio-code 
+
+# --- AI models and tools ---
+brew install gemini-cli
+brew install --cask claude claude-code 
+
+# --- Web and cloud storage ---
+brew install --cask proton-drive google-drive google-chrome
+
+# --- Notes and documentation ---
+brew install openspec mkdocs-material
 brew install --cask zotero granola
 
-# System
-brew install --cask chronosync rectangle google-chrome google-drive
+# --- Audio ---
+brew install --cask audio-hijack loopback soundsource
 
-
-###############################################################
-# Fonts
+# --- Fonts ---
 brew install --cask font-open-sans font-dm-sans
 brew install --cask font-ia-writer-duo font-ia-writer-mono font-ia-writer-quattro
 
